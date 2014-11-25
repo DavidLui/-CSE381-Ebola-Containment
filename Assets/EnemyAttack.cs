e1f70ed170401fe32b12303b53206ec1b0cc4656
ooweirdoo <maxever4@yahoo.com>
2014-11-24 21:07:56 -0500

    Update @ 9pm

--- Assets/EnemyAttack.cs
+++ Assets/EnemyAttack.cs
@@ -28,28 +28,30 @@ public class EnemyAttack : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		timer += Time.deltaTime;
-		float distance = Vector3.Distance (Player.transform.position, transform.position);
-		if (playerHealth.currentHealth <= 0) {
-			Aggro = false;
-		}
-		else if (distance <= 10) {
-			Aggro = true;
-		}
-		if (Aggro == true) {
-			if(EnemyHealth.currentHealth > 0 && distance >= 11)
-			{
-				rigidbody.velocity = transform.forward * 3.0f;
-			}
-			else {
-				rigidbody.velocity = transform.forward * 0.0f;
-			}
-			
-			if ((timer>= timeBetweenBullets) && EnemyHealth.currentHealth > 0) {
-				Shoot();
-				Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
-			}
-		}
+		if (Application.loadedLevel == 1) {
+						timer += Time.deltaTime;
+						float distance = Vector3.Distance (Player.transform.position, transform.position);
+						if (playerHealth.currentHealth <= 0) {
+								Aggro = false;
+						} else if (distance <= 10) {
+								Aggro = true;
+						}
+						if (Aggro == true) {
+								if (EnemyHealth.currentHealth > 0 && distance >= 11) {
+										rigidbody.velocity = transform.forward * 3.0f;
+								} else {
+										rigidbody.velocity = transform.forward * 0.0f;
+								}
+				
+								if ((timer >= timeBetweenBullets) && EnemyHealth.currentHealth > 0) {
+										Shoot ();
+										Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+								}
+						}
+				} else if (Application.loadedLevel == 5) {
+					rigidbody.velocity = transform.forward * 5.0f;
+
+				}
 		if (Player != null) {
 			transform.LookAt(Player.transform);
 		}
